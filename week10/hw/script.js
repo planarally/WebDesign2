138adba7b85157734a5b055532b1a064425af614
Maddison Nielsen <maddison.nielsen@gmail.com>
2021-05-03 16:55:44 -0700

    with blood sweat and tears i have finished the homework

---
+++ week10/hw/script.js
@@ -0,0 +1,33 @@
+//variables
+let menuIMG = document.getElementById("menuimage");
+let menuTGGL = document.getElementById("menutoggle");
+
+//navigation
+let toggleTheImage = function(){
+
+    if(menuTGGL.checked){
+        console.log("open");
+        menuIMG.src = "menuopen.png";
+    }
+    else {
+        console.log("closed");
+        menuIMG.src = "menuclosed.png";
+    }
+}
+
+//gallery
+function myFunction(imgs) {
+    // Get the expanded image
+    var expandImg = document.getElementById("expandedImg");
+    // Get the image text
+    var imgText = document.getElementById("imgtext");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg.src = imgs.src;
+    // Use the value of the alt attribute of the clickable image as text inside the expanded image
+    imgText.innerHTML = imgs.alt;
+    // Show the container element (hidden with CSS)
+    expandImg.parentElement.style.display = "block";
+  } 
+
+
+  menuTGGL.addEventListener("change", toggleTheImage);
