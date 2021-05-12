8ca1563785d0bdf98c72e6e9b228ed26c26d40da
Maddison Nielsen <maddison.nielsen@gmail.com>
2021-05-11 19:15:05 -0700

    fixed based on in-class feedback

--- week11/hw/script.js
+++ week11/hw/script.js
@@ -1,4 +1,4 @@
-//carousel:
+//arrays:
 let grassCute = ["img/bulbasaur.jpg", "img/tangela.jpg", "img/bellsprout.jpg"];
 let grassScary = ["img/rillaboom.jpg", "img/rayquaza.jpg", "img/zarude.jpg"];
 let fireWeak = ["img/charmeleon.jpg", "img/litten.jpg", "img/vulpix.jpg"];
@@ -7,36 +7,153 @@ let waterSmall = ["img/dratini.jpg", "img/poliwhirl.jpg", "img/squirtle.jpg"];
 let waterBig = ["img/greninja.jpg", "img/kygore.jpg", "img/swampert.jpg"];
 
 let currentIndex = 2;
+
 let carouselIMG = document.getElementById("carouselIMG");
 let groupSelect = document.getElementById("groupSelect");
 let categorySelect= document.getElementById("categorySelect");
+
 let grassOptions = ['Cute', 'Scary'];
 let fireOptions = ['Weak', 'Strong'];
 let waterOptions = ['Small', 'Big'];
 
-//carousel:
-    let changeSlide = function(){
-         if(currentIndex === 0){
-                carouselIMG.src = fireWeak[1];
-                currentIndex = 1;
-            }
-        else if(currentIndex === 1){
-                carouselIMG.src = fireWeak[2];
-                currentIndex = 2;
-            }
-        else {
-                carouselIMG.src = fireWeak[0];
-                currentIndex = 0;
-            }
+//carousels:
+let changeGrassCute = function(){
+    if(currentIndex === 0){
+        carouselIMG.src = grassCute[1];
+        currentIndex = 1;
+    }
+    else if(currentIndex === 1){
+        carouselIMG.src = grassCute[2];
+        currentIndex = 2;
+    }
+    else {
+        carouselIMG.src = grassCute[0];
+        currentIndex = 0;
+    }
 
-        if (currentIndex === 2){
-                currentIndex=0;
-            }
-        else{
-            currentIndex =  currentIndex+1;
-            }
+    if (currentIndex === 2){
+        currentIndex=0;
+    }
+    else{
+    currentIndex =  currentIndex+1;
+    }
 
-        }
+}
+
+let changeGrassScary = function(){
+    if(currentIndex === 0){
+        carouselIMG.src = grassScary[1];
+        currentIndex = 1;
+    }
+    else if(currentIndex === 1){
+        carouselIMG.src = grassScary[2];
+        currentIndex = 2;
+    }
+    else {
+        carouselIMG.src = grassScary[0];
+        currentIndex = 0;
+    }
+
+    if (currentIndex === 2){
+        currentIndex=0;
+    }
+    else{
+    currentIndex =  currentIndex+1;
+    }
+
+}
+
+let changeWaterBig = function(){
+    if(currentIndex === 0){
+        carouselIMG.src = waterBig[1];
+        currentIndex = 1;
+    }
+    else if(currentIndex === 1){
+        carouselIMG.src = waterBig[2];
+        currentIndex = 2;
+    }
+    else {
+        carouselIMG.src = waterBig[0];
+        currentIndex = 0;
+    }
+
+    if (currentIndex === 2){
+        currentIndex=0;
+    }
+    else{
+    currentIndex =  currentIndex+1;
+    }
+
+}
+
+let changeWaterSmall = function(){
+    if(currentIndex === 0){
+        carouselIMG.src = waterSmall[1];
+        currentIndex = 1;
+    }
+    else if(currentIndex === 1){
+        carouselIMG.src = waterSmall[2];
+        currentIndex = 2;
+    }
+    else {
+        carouselIMG.src = waterSmall[0];
+        currentIndex = 0;
+    }
+
+    if (currentIndex === 2){
+        currentIndex=0;
+    }
+    else{
+    currentIndex =  currentIndex+1;
+    }
+
+}
+
+let changeFireWeak = function(){
+    if(currentIndex === 0){
+        carouselIMG.src = fireWeak[1];
+        currentIndex = 1;
+    }
+    else if(currentIndex === 1){
+        carouselIMG.src = fireWeak[2];
+        currentIndex = 2;
+    }
+    else {
+        carouselIMG.src = fireWeak[0];
+        currentIndex = 0;
+    }
+
+    if (currentIndex === 2){
+        currentIndex=0;
+    }
+    else{
+    currentIndex =  currentIndex+1;
+    }
+
+}
+
+let changeFireStrong = function(){
+    if(currentIndex === 0){
+        carouselIMG.src = fireStrong[1];
+        currentIndex = 1;
+    }
+    else if(currentIndex === 1){
+        carouselIMG.src = fireStrong[2];
+        currentIndex = 2;
+    }
+    else {
+        carouselIMG.src = fireStrong[0];
+        currentIndex = 0;
+    }
+
+    if (currentIndex === 2){
+        currentIndex=0;
+    }
+    else{
+    currentIndex =  currentIndex+1;
+    }
+
+}
 
 //category
 let updateCategory = function(){
@@ -64,163 +181,41 @@ let updateCategory = function(){
 let toggleCarousel = function(){
     let selectedGroup = groupSelect.value;
     let selectedCategory = categorySelect.value;
-    if (selectedGroup === "grass", selectedCategory === "Cute"){
+
+    if (selectedCategory === "Cute"){
         console.log("cute grass"); 
-        changeSlide;{
-            if(currentIndex === 0){
-                carouselIMG.src = grassCute[1];
-                currentIndex = 1;
-            }
-            else if(currentIndex === 1){
-                carouselIMG.src = grassCute[2];
-                currentIndex = 2;
-            }
-            else {
-                carouselIMG.src = grassCute[0];
-                currentIndex = 0;
-            }
-
-            if (currentIndex === 2){
-                currentIndex=0;
-            }
-            else{
-            currentIndex =  currentIndex+1;
-            }
-        }
+        changeGrassCute();
     }
 
-    else if (selectedGroup === "grass", selectedCategory === "Scary"){
+    else if (selectedCategory === "Scary"){
         console.log("scary grass");
-        changeSlide;{
-            if(currentIndex === 0){
-                carouselIMG.src = grassScary[1];
-                currentIndex = 1;
-            }
-            else if(currentIndex === 1){
-                carouselIMG.src = grassScary[2];
-                currentIndex = 2;
-            }
-            else {
-                carouselIMG.src = grassScary[0];
-                currentIndex = 0;
-            }
-
-            if (currentIndex === 2){
-                currentIndex=0;
-            }
-            else{
-            currentIndex =  currentIndex+1;
-            }
-        }
-
+        changeGrassScary();
     }
 
-    else if (selectedGroup === "water", selectedCategory === "Small"){
+    else if (selectedCategory === "Small"){
         console.log("small water");
-        changeSlide;{
-            if(currentIndex === 0){
-                carouselIMG.src = waterSmall[1];
-                currentIndex = 1;
-            }
-            else if(currentIndex === 1){
-                carouselIMG.src = waterSmall[2];
-                currentIndex = 2;
-            }
-            else {
-                carouselIMG.src = waterSmall[0];
-                currentIndex = 0;
-            }
-
-            if (currentIndex === 2){
-                currentIndex=0;
-            }
-            else{
-            currentIndex =  currentIndex+1;
-            }
-        }
-
+        changeWaterSmall();    
     }
 
-    else if (selectedGroup === "water", selectedCategory === "Big"){
+    else if (selectedCategory === "Big"){
         console.log("big water");
-        changeSlide;{
-            if(currentIndex === 0){
-                carouselIMG.src = waterBig[1];
-                currentIndex = 1;
-            }
-            else if(currentIndex === 1){
-                carouselIMG.src = waterBig[2];
-                currentIndex = 2;
-            }
-            else {
-                carouselIMG.src = waterBig[0];
-                currentIndex = 0;
-            }
-
-            if (currentIndex === 2){
-                currentIndex=0;
-            }
-            else{
-            currentIndex =  currentIndex+1;
-            }
-        }
+        changeWaterBig();
     }
 
-    else if (selectedGroup === "fire", selectedCategory === "Weak"){
+    else if (selectedCategory === "Weak"){
         console.log("weak fire");
-        changeSlide;{
-            if(currentIndex === 0){
-                carouselIMG.src = fireWeak[1];
-                currentIndex = 1;
-            }
-            else if(currentIndex === 1){
-                carouselIMG.src = fireWeak[2];
-                currentIndex = 2;
-            }
-            else {
-                carouselIMG.src = fireWeak[0];
-                currentIndex = 0;
-            }
-
-            if (currentIndex === 2){
-                currentIndex=0;
-            }
-            else{
-            currentIndex =  currentIndex+1;
-            }
-        }
+        changeFireWeak();
     }
     
-    else if (selectedGroup === "fire", selectedCategory === "Strong"){
-        console.log("strong fire");
-        changeSlide;{
-            if(currentIndex === 0){
-                carouselIMG.src = fireStrong[1];
-                currentIndex = 1;
-            }
-            else if(currentIndex === 1){
-                carouselIMG.src = fireStrong[2];
-                currentIndex = 2;
-            }
-            else {
-                carouselIMG.src = fireStrong[0];
-                currentIndex = 0;
-            }
-
-            if (currentIndex === 2){
-                currentIndex=0;
-            }
-            else{
-            currentIndex =  currentIndex+1;
-            }
-        }
-        
+    else if (selectedCategory === "Strong"){
+        console.log("weak fire");
+        changeFireWeak();
     }
 }
 
 //events
 window.addEventListener("load", function(){
-    setInterval(changeSlide, 5000)
+    setInterval(toggleCarousel, 4000);
 })
 
 groupSelect.addEventListener("change", updateCategory);
